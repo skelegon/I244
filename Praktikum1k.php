809f4bde0cc3d17487cc2648754330299c5f8786
Steve Sulev <steve.sulev@itcollege.ee>
2016-02-15 23:34:20 +0200

    php counter

--- Praktikum1k.php
+++ Praktikum1k.php
@@ -1,7 +1,20 @@
 <!DOCTYPE html>
 
 <?php
-echo "hello world";
+$file = fopen("counts.txt", "r");
+
+if(!$file){
+  echo "ei saanud faili avada";
+} else {
+  $counter = (int)fread($file, 20);
+  fclose($file);
+  $counter++;
+
+  $handle = fopen("counts.txt", "w");
+  fwrite($handle, $counter);
+  fclose($handle);
+}
+
 ?>
 
 <html>
@@ -45,7 +58,9 @@ echo "hello world";
             <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
             </a>
             <br />
-            <div id="txt"></div>
+            <div class="inlinediv">
+            <div id="txt"></div><div><?=", Visitors: " . $counter;?></div>
+          </div>
          </div>
       </div>
    </body>
